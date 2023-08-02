b8311e552c7e26df46df336151e16b663d6d9221
Matheus <matheus.ms2002@hotmail.com>
2023-08-01 22:27:20 -0300

    models

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+//fornecedor
+//fornecedors
+
 class Fornecedor extends Model
 {
     //
+    protected $table = 'fornecedores';
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
